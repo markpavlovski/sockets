67edefb16b6d8180510550103146e2e8d6016127
Mark Pavlovski <markpavlovski@gmail.com>
2018-04-08 20:08:35 -0700

    added received image status

--- index.js
+++ index.js
@@ -16,11 +16,19 @@ io.on('connection', function(socket){
   socket.on('ready message', function(){
     console.log('received ready message');
   });
+  // fs.readFile(path.resolve(__dirname, './image.jpg'), function(err, data){
+  //   socket.emit('imageConversionByClient', { image: true, buffer: data })
+  //   socket.emit('imageConversionByServer', "data:image/png;base64,"+ data.toString("base64"))
+  // })
+  sendImage(socket)
+})
+
+function sendImage(socket){
   fs.readFile(path.resolve(__dirname, './image.jpg'), function(err, data){
     socket.emit('imageConversionByClient', { image: true, buffer: data })
     socket.emit('imageConversionByServer', "data:image/png;base64,"+ data.toString("base64"))
   })
-})
+}
 
 
 
