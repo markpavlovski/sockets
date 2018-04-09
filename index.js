b558abc754e7efc0b509c38c8af101047e2e36b0
Mark Pavlovski <markpavlovski@gmail.com>
2018-04-08 20:02:27 -0700

    removed all jquery

--- index.js
+++ index.js
@@ -11,23 +11,14 @@ app.get('/', function(req,res){
   res.sendFile(__dirname + '/image.html')
 })
 
+
 io.on('connection', function(socket){
   fs.readFile(path.resolve(__dirname, './image.jpg'), function(err, data){
-    socket.emit('imageConversionByClient', { image: true, buffer: data });
-    socket.emit('imageConversionByServer', "data:image/png;base64,"+ data.toString("base64"));
-  });
-});
+    socket.emit('imageConversionByClient', { image: true, buffer: data })
+    socket.emit('imageConversionByServer', "data:image/png;base64,"+ data.toString("base64"))
+  })
+})
 
-// 
-// io.on('connection', function(socket){
-//   socket.on('chat message', function(msg){
-//     console.log(msg);
-//     setInterval(()=>{
-//       io.emit('chat message', msg);
-//     },1000)
-//
-//   });
-// });
 
 
 http.listen(3000, function(){
